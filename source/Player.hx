5b7d74b71f1cbf13a4d92bd6190e8ba32cd6a3bf
Matthew Richard <82064170+matthewjRichard@users.noreply.github.com>
2023-10-17 21:23:48 -0400

    Added and scaled player sprite

--- source/Player.hx
+++ source/Player.hx
@@ -4,24 +4,25 @@ import flixel.FlxG;
 import flixel.util.FlxColor;
 import flixel.FlxSprite;
 
-class Player extends FlxSprite
-{
-    public var pHealth:Int;
-    public var bullet:FlxSprite;
-    public var shootingEnabled:Bool;
+class Player extends FlxSprite {
+	public var pHealth:Int;
+	public var bullet:FlxSprite;
+	public var shootingEnabled:Bool;
 
-    public function new(x:Float = 0, y:Float = 0, color:FlxColor = FlxColor.BLUE, playerNum:Int = 0)
-    {
-        super(x,y);
+	public function new(x:Float = 0, y:Float = 0, color:FlxColor = FlxColor.BLUE, playerNum:Int = 0) {
+		super(x, y);
 
-        // Dynamically load character sprite
-        this.loadGraphic('assets/images/character${playerNum}.png');
+		// Dynamically load character sprite
+		this.loadGraphic('assets/images/character${playerNum}.png');
+		this.scale.x = 0.06;
+		this.scale.y = 0.06;
+		this.updateHitbox();
 
-        // Set player attributes
-        pHealth = 100;
-        bullet = new FlxSprite(FlxG.width/2-5, FlxG.height-30);
-        bullet.makeGraphic(9, 2, color, true);
-        bullet.visible = false;
-        shootingEnabled = true;
-    }
+		// Set player attributes
+		pHealth = 100;
+		bullet = new FlxSprite(FlxG.width / 2 - 5, FlxG.height - 30);
+		bullet.makeGraphic(9, 2, FlxColor.WHITE, true);
+		bullet.visible = false;
+		shootingEnabled = true;
+	}
 }
