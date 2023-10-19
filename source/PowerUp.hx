3cab98ad381ded62fa31796c79450f00d47ddde6
Matthew Richard <82064170+matthewjRichard@users.noreply.github.com>
2023-10-19 17:46:18 -0400

    Fixed powerup scaling

--- source/PowerUp.hx
+++ source/PowerUp.hx
@@ -5,7 +5,8 @@ class PowerUp extends FlxSprite {
     public function new(x:Float, y:Float) {
         super(x, y);
         this.loadGraphic("assets/images/heart.png");
-        this.scale.set(0.06,0.06);
+        this.scale.x = 1.25;
+		this.scale.y = 1.25;
         this.updateHitbox();
     }
 
