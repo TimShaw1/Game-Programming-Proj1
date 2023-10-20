17a0a167c1c1c412cb3a99e6d05411b26fa82773
Tim Shaw <timothyshaw0225@gmail.com>
2023-10-19 20:14:19 -0400

    add documentation

--- source/Asteroid.hx
+++ source/Asteroid.hx
@@ -15,23 +15,27 @@ class Asteroid extends FlxSprite
         this.loadGraphic("assets/images/asteroid.png");
     }
 
-    // TODO: Random scaling
+    /**
+     * Places the asteroid in a random spot off-screen. Applies speed and scaling as well.
+     */
     public function set_up()
     {
+        // Spawn in random spot off screen
         var x = Math.random() * FlxG.width;
 		var y = Math.random() < 0.5 ? -100 : FlxG.height + 100;
-		if (x > FlxG.width / 2)
-			x *= -1;
 
+        // Apply random velocity
 		var xVel = Math.random() * 50 + 1;
 		var yVel = Math.random() * 50 + 1;
 		if (y > 0)
 			yVel *= -1;
+        if (x > FlxG.width / 2)
+			xVel *= -1;
 
 		this.x = x;
 		this.y = y;
 
-        // Trends larger as game progresses
+        // Randomly scale asteroid - Trends larger as game progresses
         this.setGraphicSize(Math.round(this.width * (Math.random() + 1)), 0);
 
 		this.velocity.x = xVel;
